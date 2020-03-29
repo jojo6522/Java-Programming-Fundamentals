d0b080c22418f13d8d9163eba1ee2214db766e90
jojo6522 <61921257+jojo6522@users.noreply.github.com>
2020-03-29 17:07:09 -0400

    Create Exercise8.java

---
+++ Java Text Daniel Liang/Chapter7/Exercise8.java
@@ -0,0 +1,12 @@
+/*
+
+Write two overloaded methods that return the sum of an array with the following headers:
+
+public static int sum(int[] array)
+public static double sum(double[] array)
+
+Write a test program that prompts the user to enter ten doubles values, invokes this method, and displays the sum value.
+*/
+
+importjava.util.Scanner;
+
