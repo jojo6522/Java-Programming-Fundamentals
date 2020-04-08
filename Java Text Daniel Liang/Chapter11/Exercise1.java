8809692ec545b4975cb173c000c1ac6f5f84ffa4
jojo6522 <61921257+jojo6522@users.noreply.github.com>
2020-04-08 13:48:01 -0400

    Update Exercise1.java

--- Java Text Daniel Liang/Chapter11/Exercise1.java
+++ Java Text Daniel Liang/Chapter11/Exercise1.java
@@ -42,3 +42,83 @@ public class GeometricObject {
 }
 
 //Triangle Class
+
+public class Triangle extends GeometricObject {
+
+    private double side1, side2, side3;
+
+    public Triangle(){
+        super();
+        side1=1;
+        side2=1;
+        side3=1;
+    }
+
+    public Triangle(double one, double two, double three, String colour, boolean filled){
+        super(colour,filled);
+
+        side1=one;
+        side2=two;
+        side3=three;
+    }
+
+    public void setSide1(double side1) {
+        this.side1 = side1;
+    }
+
+    public void setSide2(double side2) {
+        this.side2 = side2;
+    }
+
+    public void setSide3(double side3) {
+        this.side3 = side3;
+    }
+
+    public double getSide1() {
+        return side1;
+    }
+
+    public double getSide2() {
+        return side2;
+    }
+
+    public double getSide3() {
+        return side3;
+    }
+
+    public double getArea()
+    {
+        return side1 * side2;
+    }
+
+    public double getPerimeter()
+    {
+        return side1+side2+side3;
+    }
+
+    public String toString()
+    {
+
+        return " The sides of the triangle are " + this.getSide1() +  " " +this.getSide2() + " " + this.getSide3()
+                + " \n the area and perimeter is " + this.getArea() + " " + this.getPerimeter() + super.toString();
+
+
+    }
+
+    public static void main(String[] args)
+    {
+        //super("green", true);
+        Triangle t1 = new Triangle(15,13,25, "green", true );
+        System.out.println(t1);
+
+
+    }
+
+
+
+
+}
+
+
+
+
